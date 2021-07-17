2ebe89533a73ded776bc1586445848e09aca2b23
Mohamed Shathik <mohamedshafa17@gmail.com>
2021-07-18 00:11:54 +0530

    init

--- GIVI task1.py
+++ GIVI task1.py
@@ -6,27 +6,35 @@ mydb=mysql.connector.connect(
     database="learning"
     )
 cursor=mydb.cursor()
-A=input("Enter Your Name ")
-B=input("Enter your department ")
-m1=int(input("Enter mark1 "))
-m2=int(input("Enter mark2 "))
-m3=int(input("Enter mark3 "))
-m4=int(input("Enter mark4 "))
-m5=int(input("Enter mark5 "))
-total=(m1+m2+m3+m4+m5)
-avg=(total/5)
-if avg<=100 and avg>=90:
-    grade="S"
-elif avg<90 and avg>=80:
-    grade="A"
-elif avg<80 and avg>=70:
-    grade="B"
-elif avg<70 and avg>=60:
-    grade="C"
-else:
-    grade="D"
-
-cursor.execute("CREATE TABLE student (studentid int PRIMARY key AUTO_INCREMENT not null,Name varchar(20) not null, Department varchar(20) not null,mark1 int not null, mark2 int not null, mark3 int not null, mark4 int not null,mark5 int not null, totalmark int not null, average int not null, grade varchar(5))")
+print("""MYsql details registration
+1. add new student
+2. get student
+3. get all student
+4. edit a student
+5. delete a student
+6. exit
+""")
+choice=int(input("enter your choice"))
+if choice==1:
+    A=input("Enter Your Name ")
+    B=input("Enter your department ")
+    m1=int(input("Enter mark1 "))
+    m2=int(input("Enter mark2 "))
+    m3=int(input("Enter mark3 "))
+    m4=int(input("Enter mark4 "))
+    m5=int(input("Enter mark5 "))
+    total=(m1+m2+m3+m4+m5)
+    avg=(total/5)
+    if avg<=100 and avg>=90:
+        grade="S"
+    elif avg<90 and avg>=80:
+        grade="A"
+    elif avg<80 and avg>=70:
+        grade="B"
+    elif avg<70 and avg>=60:
+        grade="C"
+    else:
+        grade="D"
 
 
 s="INSERT INTO student(Name, Department, mark1, mark2, mark3, mark4, mark5, totalmark, average, grade) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
